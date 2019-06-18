778beab67afdbfce2f9bf9283dd74eff337dc9b7
youchuang9 <992420338@qq.com>
2019-06-18 19:00:55 +0800

    init

--- app/Http/Controllers/Edu/IndexController.php
+++ app/Http/Controllers/Edu/IndexController.php
@@ -9,16 +9,18 @@ use App\Model\TeacherExtend;
 use App\Model\TeacherGrade;
 use App\Model\TeacherSubject;
 use App\Model\TeacherSignLog;
+use App\Model\GoodsServer;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class IndexController extends BaseController
 {
     // 查看日志
-    public function getSign(Request $req,TeacherSignLog $teacher_sign_log,TeacherExtend $teacher_extend,TeacherGrade $teacher_grade,TeacherSubject $teacher_subject){
+    public function getSign(Request $req,TeacherSignLog $teacher_sign_log,TeacherExtend $teacher_extend,TeacherGrade $teacher_grade,TeacherSubject $teacher_subject,GoodsServer $goods_server){
         $time = date('Y-m-d');
         $userInfo = JWTAuth::parseToken()->touser();
-        $data['today'] = $teacher_sign_log->where(['user_id'=>$userInfo['id'],'format_time'=>$time])->exists();
-        $data['count'] = count($teacher_sign_log->select('id','format_time')->where(['user_id'=>$userInfo['id']])->groupBy('format_time')->get());
+        $data['server'] = $goods_server->where('user_id',28)->first();
+        // $data['today'] = $teacher_sign_log->where(['user_id'=>$userInfo['id'],'format_time'=>$time])->exists();
+        // $data['count'] = count($teacher_sign_log->select('id','format_time')->where(['user_id'=>$userInfo['id']])->groupBy('format_time')->get());
         $extend = $teacher_extend->where('user_id',$userInfo['id'])->first();
         $grade = $teacher_grade->find($extend['grade_id']);
         $subject = $teacher_subject->find($extend['subject_id']);
